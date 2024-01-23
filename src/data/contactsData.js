b2c67815cb5bd8c6480ec9e3fd5fdc1a02bb1736
Vishal Jadaun <vishaljadaun@metamix.tech>
2024-01-23 13:49:35 +0530

    enicial commit

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
   email: "jadaunvishal73@gmail.com",
   phone: "+917351870136",
   address: "Sector 10A gurugram",
-  github: "https://github.com/jadaunGitvisha",
+  github: "https://github.com/jadaunGitvishal",
   //   facebook: "https://www.facebook.com/abusaid.riyaz/",
   linkedIn: "https://www.linkedin.com/in/vishal-jadaun-a801b5182/",
   //   twitter: "https://twitter.com/said7388",
